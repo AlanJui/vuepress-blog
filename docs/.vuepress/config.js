619888cd6b310e0a60978b22ed69d2dd6eae2425
居正中 <AlanJui.1960@gmail.com>
2019-10-20 15:45:40 +0800

    完成初版

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '中道部落格',
+  description: '使用 VuePress 打造的部落格',
+  themeConfig: {
+    nav: [
+      { text: '首頁', link: '/' },
+      { text: '指引', link: '/guide/' },
+      { text: '文章', link: '/posts/' },
+      { text: '模版', link: 'https://github.com/CheolHoJung/vuepress-blog-boilerplate' },
+    ],
+    sidebar: [
+      '/',
+      '/guide/',
+      '/posts/',
+    ]
+  }
+}
